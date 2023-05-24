a9cdea327fa0b5206c6257d4788c37e3e5fa2b39
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-05-24 16:59:45 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,27 @@
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == b == c:
+print(3)
+elif a == b or a == c or b == c:
+print(2)
+else:
+print(0)
+
+print((5 + (7 * 5) / 8) / (3**5))
+2
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x1 == x2) or (y1 == y2):
+print("YES")
+else:
+print("NO")
+2
+v = int(input())
+t = int(input())
+distance = v * t
+print(distance)
